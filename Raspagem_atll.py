4a92ad5e11bf488fe0f8d69e54516a08483f4d85
Ariel-Developer <112713200+Ariel-Developer@users.noreply.github.com>
2022-09-24 14:16:49 -0300

    Update Raspagem_atll.py

--- Raspagem_atll.py
+++ Raspagem_atll.py
@@ -11,16 +11,22 @@ import pandas as pd
 import smtplib
 import email.message
 
+
 class raspagem:
     def __init__(self):
         self.login_email()
         self.raspagem_de_dados()
         self.enviando_dados_to_Database()
         self.enviando_to_email()
+
     def login_email(self):
         self.email_usuario = input(
             '\033[1;92mdigite o seu email para receber o conteúdo:'
         )
+        self.email_enviar = input(
+            '\033[1;92mDigite o email que íra receber: '
+        )
+
     def raspagem_de_dados(self):
         self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         self.driver.get("https://lista.mercadolivre.com.br/celular#D[A:celular]")
@@ -33,6 +39,7 @@ class raspagem:
         self.celular = self.site.findAll("li", class_="ui-search-layout__item shops__layout-item")
         for i in tqdm(range(2)):
             time.sleep(1)
+
     def enviando_dados_to_Database(self):
         for self.celulares in self.celular:
             self.marca = self.celulares.find("h2", class_="ui-search-item__title shops__item-title").get_text().strip()
@@ -46,16 +53,16 @@ class raspagem:
             self.Data.commit()
             self.Data.close()
         print(self.tabela)
+
     def enviando_to_email(self):
         corpo_email = f"""
-           <p>Olá {self.email_usuario}</p>
            <p>Tabela dos celulares</p>
            <p>Planilha {self.tabela.to_html()}</p>
            """
         msg = email.message.Message()
         msg["Subject"] = "Dados coletados"
-        msg["From"] = "{self.email_usuario}"
-        msg["To"] = "Pessoa"
+        msg["From"] = self.email_usuario
+        msg["To"] = self.email_enviar
         password = "zcfqerlqwptircbn"
         msg.add_header("Content-Type", "text/html")
         msg.set_payload(corpo_email)
@@ -64,5 +71,6 @@ class raspagem:
         s.login(msg["from"], password)
         s.sendmail(msg["From"], [msg["To"]], msg.as_string().encode("utf-8"))
         print("\033[1;92mEmail enviado com sucesso!!")
+
 
 rp = raspagem()
