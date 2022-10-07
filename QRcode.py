9041db90b4cb3384d41f962e6ffb7fe1bc6fd50b
Ariel-Developer <112713200+Ariel-Developer@users.noreply.github.com>
2022-10-07 14:14:29 -0300

    Add files via upload

---
+++ QRcode.py
@@ -0,0 +1,20 @@
+from tkinter import *
+import qrcode
+def qrcode1(event):
+    root.geometry("500x400")
+    qr = qrcode.make(entry.get())
+    qr.save("qrcodeYT.jpg")
+    entry.destroy()
+    pht = PhotoImage(file="qrcodeYT.jpg")
+    label = Label(image=pht)
+    label.pack()
+
+root = Tk()
+root.geometry("300x100")
+entry = Entry()
+entry.place(x=50, y=50, width=180)
+entry.bind("<Return>", qrcode1)
+root.mainloop()
+
+
+
